4da22b7a2de2488f6f2fdc13174ee43763d46b32
JordanVScher <jordanvscher@hotmail.com>
2019-05-14 16:29:53 -0300

    Remove ma_api

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ const request = require('request');
 //const dialogFlow = require('apiai-promise');
 
 const config = require('./bottender.config.js').messenger;
-const FrontAPI = require('./mandatoaberto_api.js');
+// const FrontAPI = require('./mandatoaberto_api.js');
 const opt = require('./utils/options');
 const { createIssue } = require('./send_issue');
 const { Sentry } = require('./utils/helper');
@@ -17,21 +17,22 @@ const googleMapsClient = require('@google/maps').createClient({
 	Promise,
 });
 
-const mapPageToAccessToken = async (pageId) => {
-	const perfilData = await FrontAPI.getPoliticianData(pageId);
-	return config.accessToken;
-	// return perfilData.fb_access_token ? perfilData.fb_access_token : process.env.ACCESS_TOKEN;
-};
+// const mapPageToAccessToken = async (pageId) => {
+// 	const perfilData = await FrontAPI.getPoliticianData(pageId);
+// 	return config.accessToken;
+// 	// return perfilData.fb_access_token ? perfilData.fb_access_token : process.env.ACCESS_TOKEN;
+// };
 
 const bot = new MessengerBot({
-	mapPageToAccessToken,
+	// mapPageToAccessToken,
+	accessToken: config.accessToken,
 	appSecret: config.appSecret,
 	sessionStore: new FileSessionStore(),
 });
 
 bot.setInitialState({});
 
-bot.use(withTyping({ delay: 1000 * 2 }));
+// bot.use(withTyping({ delay: 1000 * 2 }));
 
 async function waitTypingEffect(context) {
 	await context.typingOn();
@@ -87,8 +88,7 @@ const handler = new MessengerHandler()
 					await waitTypingEffect(context);
 					context.setState({ dialog: 'Não, voltar'})
 				}
-			}
-			else if ((context.state.dialog === 'Digitar a cidade' || context.state.dialog === 'Cidade errada') && context.event.isText) {
+			}	else if ((context.state.dialog === 'Digitar a cidade' || context.state.dialog === 'Cidade errada') && context.event.isText) {
 				let cidadeTyped = context.event.message.text;
 				let result = await findCidade(cidadeTyped);
 				if (result) {
@@ -97,21 +97,9 @@ const handler = new MessengerHandler()
 				else {
 					await context.setState({ dialog: "Cidade errada" })
 				}
-			}
-			//let user = await getUser(context)
-			// we reload politicianData on every useful event
-			// we update context data at every interaction that's not a comment or a post
-			await context.setState({ politicianData: await FrontAPI.getPoliticianData(context.event.rawEvent.recipient.id) });
-			await FrontAPI.postRecipient(context.state.politicianData.user_id, {
-				fb_id: context.session.user.id,
-				name: `${context.session.user.first_name} ${context.session.user.last_name}`,
-				origin_dialog: 'greetings',
-				picture: context.session.user.profile_pic,
-				session: JSON.stringify(context.state),
-			});
-			if (context.event.isText) {
+			} else if (context.event.isText && !context.event.isQuickReply) {
 				await context.setState({ whatWasTyped: context.event.message.text }); // has to be set here because of talkToUs
-				await createIssue(context, 'Não entendi sua mensagem pois ela é muito complexa. Você pode escrever novamente, de forma mais direta?')
+				await context.sendText('Não entendi sua mensagem. Por favor, utilize os botões.');
 			}
 
 		// Tratando dados adicionais do recipient
@@ -128,11 +116,6 @@ const handler = new MessengerHandler()
 		if (context.state.dialog === 'recipientData' && context.state.recipientData) {
 			if (context.state.recipientData === 'email') {
 				await context.setState({dialog: 'waiting', time: Date.now()})
-				await FrontAPI.postRecipient(context.state.politicianData.user_id, {
-					fb_id: context.session.user.id,
-					email: context.state.email,
-					session: JSON.stringify(context.state)
-				});
 				await context.sendText(`Obrigada 👍\nVeja o e-mail que iremos enviar:`);
 				await waitTypingEffect(context);
 				await context.sendText(`[texto sobre o cenário da cidade]\n[texto sobre a pesquisa]`, { quick_replies: opt.Pressionar2 });
@@ -274,19 +257,6 @@ const handler = new MessengerHandler()
 		console.log('\n');
 		console.log(`Parece que aconteceu um erro as ${date.toLocaleTimeString('pt-BR')} de ${date.getDate()}/${date.getMonth() + 1} =>`);
 		console.log(err);
-		// if (context.event.rawEvent.field === 'feed') {
-		// 	if (context.event.rawEvent.value.item === 'comment' || context.event.rawEvent.value.item === 'post') {
-		// 		// we update user data at every interaction that's not a comment or a post
-		// 		await context.setState({ politicianData: await FrontAPI.getPoliticianData(context.event.rawEvent.recipient.id) });
-		// 		await context.setState({ pollData: await FrontAPI.getPollData(context.event.rawEvent.recipient.id) });
-		// 	}
-		// } else {
-		await context.setState({ politicianData: await FrontAPI.getPoliticianData(context.event.rawEvent.recipient.id) });
-		await context.setState({ pollData: await FrontAPI.getPollData(context.event.rawEvent.recipient.id) });
-		// }
-
-		// console.log('\n\n\n\nrawEvent.recipient.id no catch', context.event.rawEvent.recipient.id);
-		// console.log('politicianData no catch', context.state.politicianData);
 
 		await Sentry.configureScope(async (scope) => {
 			if (context.session.user && context.session.user.first_name && context.session.user.last_name) {
@@ -296,14 +266,7 @@ const handler = new MessengerHandler()
 				scope.setUser({ username: 'no_user' });
 				console.log('Usuário => Não conseguimos descobrir o nome do cidadão');
 			}
-			if (context.state && context.state.politicianData && context.state.politicianData.name
-				&& context.state.politicianData.office && context.state.politicianData.office.name) {
-				scope.setExtra('admin', `${context.state.politicianData.office.name} ${context.state.politicianData.name}`);
-				console.log(`Administrador => ${context.state.politicianData.office.name} ${context.state.politicianData.name}`);
-			} else {
-				scope.setExtra('admin', 'no_admin');
-				console.log('Administrador => Não conseguimos descobrir o nome do político');
-			}
+
 
 			scope.setExtra('state', context.state);
 			throw err;
